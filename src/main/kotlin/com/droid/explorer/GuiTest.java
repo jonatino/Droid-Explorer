4b76ec078d791de69b2c2207a701dc82779027de
Jonathan Beaudoin <jonathanbeaudoin1996@gmail.com>
2016-04-23 04:09:22 -0400

    Fixed packaging, started tornadofx gui

--- src/main/kotlin/com/droid/explorer/GuiTest.java
+++ src/main/kotlin/com/droid/explorer/GuiTest.java
@@ -1,19 +1,12 @@
 package com.droid.explorer;
 
 import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Group;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Label;
 import javafx.stage.Stage;
 
-import javax.annotation.Resources;
 import java.io.IOException;
-import java.net.URL;
-import java.util.Map;
 
 /**
  * Created by Jonathan on 4/20/2016.
@@ -27,7 +20,7 @@ public class GuiTest extends Application {
     @Override
     public void start(Stage primaryStage) throws IOException {
 
-            Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("droidexplorer.fxml"));
+            Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("com/droid/explorer/DroidExplorer.fxml"));
 
         primaryStage.setTitle("Test FXML");
 /*        primaryStage.setWidth(320);
