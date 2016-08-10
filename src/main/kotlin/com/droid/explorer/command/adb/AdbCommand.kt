14c55c4d95cd00fe7d458989cbfb36a03a2f6482
Jonatino <jonathanbeaudoin1996@gmail.com>
2016-08-10 19:39:42 -0400

    Made adb.exe process relative

--- src/main/kotlin/com/droid/explorer/command/adb/AdbCommand.kt
+++ src/main/kotlin/com/droid/explorer/command/adb/AdbCommand.kt
@@ -1,5 +1,7 @@
 package com.droid.explorer.command.adb
 
+import com.droid.explorer.DroidExplorer
+import com.droid.explorer.command.Command
 import com.droid.explorer.command.shell.impl.Start
 import java.io.IOException
 import java.net.Socket
@@ -7,44 +9,33 @@ import java.net.Socket
 /**
  * Created by Jonathan on 4/23/2016.
  */
-abstract class AdbCommand<T> {
+abstract class AdbCommand : Command {
 
-	abstract val args: Array<String>
+    private val process by lazy { "\"${DroidExplorer::class.java.getResource("adb/adb.exe").toURI().path.substring(1)}\"" }
 
-	private val process = "E:\\Dropbox\\Droid Explorer\\src\\main\\resources\\com\\droid\\explorer\\adb\\adb.exe"
+    override fun run(block: ((String) -> Unit)?) {
+        try {
+            before()
+            val process = ProcessBuilder(process, *args).start()
+            while (!isAdbServerOnline()) {
+            }
+            if (this !is Start) {
+                process.inputStream.reader().readLines().filterNot { it.isNullOrEmpty() }.forEach {
+                    block?.invoke(it)
+                }
+            }
+        } finally {
+            after()
+        }
+    }
 
-	fun run(block: ((String) -> Unit)? = null) {
-		//println("E:\\Dropbox\\Droid Explorer\\src\\main\\resources\\com\\droid\\explorer\\adb\\adb.exe  " + Arrays.toString(args))
-		try {
-			before()
-			val process = ProcessBuilder(process, *args).start()
-			while (!isAdbServerOnline()) {
-			}
-			if (this !is Start) {
-				process.inputStream.reader().readLines().filterNot { it.isNullOrEmpty() }.forEach {
-					block?.invoke(it)
-				}
-			}
-		} finally {
-			after()
-		}
-	}
-
-	fun isAdbServerOnline(): Boolean {
-		try {
-			Socket("localhost", 5037).close()
-			return true
-		} catch(e: IOException) {
-			return false
-		}
-	}
-
-	open fun before() {
-
-	}
-
-	open fun after() {
-
-	}
+    fun isAdbServerOnline(): Boolean {
+        try {
+            Socket("localhost", 5037).close()
+            return true
+        } catch(e: IOException) {
+            return false
+        }
+    }
 
 }
