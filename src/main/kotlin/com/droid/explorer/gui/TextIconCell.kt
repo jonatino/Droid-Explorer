13d6afd0e37432a7db6ad0bf7e9cdc9d54bf0fd6
Jonatino <jonathanbeaudoin1996@gmail.com>
2016-08-10 19:47:05 -0400

    Started copy and paste system

--- src/main/kotlin/com/droid/explorer/gui/TextIconCell.kt
+++ src/main/kotlin/com/droid/explorer/gui/TextIconCell.kt
@@ -1,8 +1,11 @@
 package com.droid.explorer.gui
 
+import com.droid.explorer.Clipboard
+import com.droid.explorer.Clipboard.Mode.COPY
+import com.droid.explorer.Clipboard.Mode.CUT
+import com.droid.explorer.command.adb.impl.Pull
 import com.droid.explorer.command.adb.impl.Push
 import com.droid.explorer.command.shell.impl.Mount
-import com.droid.explorer.command.shell.impl.Pull
 import com.droid.explorer.command.shell.impl.Remove
 import com.droid.explorer.command.shell.impl.UnMount
 import com.droid.explorer.droidExplorer
@@ -20,92 +23,104 @@ import java.util.*
  */
 open class TextIconCell<T, S>() : TableCell<T, S>() {
 
-	companion object {
-		private val cache = HashMap<Int, CachedFileCell>()
-	}
+    companion object {
+        private val cache = HashMap<Int, CachedFileCell>()
+    }
 
-	override fun updateItem(item: S, empty: Boolean) {
-		super.updateItem(item, empty);
-		if (item == null || empty) {
-			text = null
-			graphic = null
-		} else {
-			val file: Entry? = tableRow.item as Entry?
-			if (file != null) {
-				val labeled = cache[file.hashCode()]
-				if (labeled == null) {
-					val rowMenu = ContextMenu()
-					when (file) {
-						is DirectoryEntry -> {
-							val open = MenuItem("Open", ImageView(Icons.OPEN.image))
-							open.setOnAction({ event -> file.navigate() })
-							rowMenu.items.addAll(open, /*MenuItem("Compress", ImageView(Icons.COMPRESS.image)),*/ SeparatorMenuItem())
-						}
-					}
+    override fun updateItem(item: S, empty: Boolean) {
+        super.updateItem(item, empty);
+        if (item == null || empty) {
+            text = null
+            graphic = null
+        } else {
+            val file: Entry? = tableRow.item as Entry?
+            if (file != null) {
+                val labeled = cache[file.hashCode()]
+                if (labeled == null) {
+                    val rowMenu = ContextMenu()
+                    when (file) {
+                        is DirectoryEntry -> {
+                            val open = MenuItem("Open", ImageView(Icons.OPEN.image))
+                            open.setOnAction { event -> file.navigate() }
+                            rowMenu.items.addAll(open, /*MenuItem("Compress", ImageView(Icons.COMPRESS.image)),*/ SeparatorMenuItem())
+                        }
+                    }
 
-					val download = MenuItem("Download", ImageView(Icons.DOWNLOAD.image))
-					download.setOnAction({
-						tableView.selectionModel.selectedItems.forEach {
-							val selectedfile = it as Entry
-							Pull(selectedfile.absolutePath, "C:/Users/Jonathan/Desktop/${selectedfile.name}").run() {
-								println(it)
-							}
-						}
-					}
-					)
+                    val download = MenuItem("Download", ImageView(Icons.DOWNLOAD.image))
+                    download.setOnAction {
+                        droidExplorer.withSelected {
+                            Pull(it.absolutePath, System.getProperty("user.home") + "\\Desktop\\${it.name}").run()
+                        }
+                    }
 
-					val move = Menu("Move")
-					val cut = MenuItem("Cut", ImageView(Icons.CUT.image))
-					val copy = MenuItem("Copy", ImageView(Icons.COPY.image))
-					val paste = MenuItem("Paste", ImageView(Icons.PASTE.image))
-					paste.isDisable = true
+                    val move = Menu("Move")
+                    val paste = MenuItem("Paste", ImageView(Icons.PASTE.image))
 
-					move.items.addAll(cut, copy, paste)
+                    val cut = MenuItem("Cut", ImageView(Icons.CUT.image))
+                    cut.setOnAction {
+                        droidExplorer.withSelected { Clipboard.content.put(it, CUT) }
+                    }
+                    val copy = MenuItem("Copy", ImageView(Icons.COPY.image))
+                    copy.setOnAction {
+                        droidExplorer.withSelected { Clipboard.content.put(it, COPY) }
+                    }
+                    paste.setOnAction {
+                        //Clipboard.moveTo(FileSystem.currentPath)
+                        Clipboard.content.forEach { entry, mode ->
+                            println("Moving $entry to ${FileSystem.currentPath.absolutePath}")
+                        }
+                    }
 
-					rowMenu.items.addAll(move, download)
+                    move.items.addAll(cut, copy, paste)
 
-					val replace = MenuItem("Upload", ImageView(Icons.UPLOAD.image))
-					replace.setOnAction({
-						val files = FileChooser().showOpenMultipleDialog(droidExplorer.primaryStage);
-						if (files != null && files.isNotEmpty()) {
-							Mount().run()
-							files.forEach { Push(it.absolutePath, FileSystem.currentPath.absolutePath).run() { println(it) } }
-							UnMount().run()
-						}
-					})
+                    rowMenu.items.addAll(move, download)
 
-					val delete = MenuItem("Delete", ImageView(Icons.DELETE.image))
+                    val upload = MenuItem("Upload", ImageView(Icons.UPLOAD.image))
+                    upload.setOnAction {
+                        val files = FileChooser().showOpenMultipleDialog(droidExplorer.primaryStage);
+                        if (files != null && files.isNotEmpty()) {
+                            Mount().run()
+                            files.forEach { Push(it.absolutePath, FileSystem.currentPath.absolutePath).run() { println(it) } }
+                            UnMount().run()
+                        }
+                    }
 
-					delete.setOnAction({
-						val alert = Alert(javafx.scene.control.Alert.AlertType.CONFIRMATION);
-						alert.title = "Confirm File Deletion";
-						alert.contentText = "Are you sure you want to delete \"$file\"? This action can NOT be reversed.";
-						alert.headerText = null
+                    val delete = MenuItem("Delete", ImageView(Icons.DELETE.image))
 
-						val result = alert.showAndWait()
-						if (result.isPresent && result.get() == ButtonType.OK) {
-							Mount().run()
-							Remove(file.absolutePath).run()
-							UnMount().run()
-						}
-					})
+                    delete.setOnAction {
+                        val alert = Alert(javafx.scene.control.Alert.AlertType.CONFIRMATION);
+                        alert.title = "Confirm File Deletion";
+                        alert.contentText = "Are you sure you want to delete \"$file\"? This action can NOT be reversed.";
+                        alert.headerText = null
+
+                        val result = alert.showAndWait()
+                        if (result.isPresent && result.get() == ButtonType.OK) {
+                            Mount().run()
+                            Remove(file.absolutePath).run()
+                            UnMount().run()
+                        }
+                    }
 
 
-					rowMenu.items.addAll(SeparatorMenuItem(), delete, replace)
+                    rowMenu.items.addAll(SeparatorMenuItem(), delete, upload)
 
-					text = file.name
-					graphic = ImageView(file.type.icon.image)
-					contextMenu = rowMenu
-					cache.put(file.hashCode(), CachedFileCell(text, graphic, contextMenu))
-				} else {
-					text = labeled.text
-					graphic = labeled.graphic
-					contextMenu = labeled.contextMenu
-				}
-			}
-		}
-	}
+                    text = file.name
+                    graphic = ImageView(file.type.icon.image)
+                    contextMenu = rowMenu
+                    cache.put(file.hashCode(), CachedFileCell(text, graphic, contextMenu, {
+                        paste.isDisable = Clipboard.content.isEmpty()
+                        println("Called")
+                    }))
+                } else {
+                    text = labeled.text
+                    graphic = labeled.graphic
+                    contextMenu = labeled.contextMenu
+                    labeled?.action.invoke()
+                }
+            }
+        }
+    }
 }
 
-data class CachedFileCell(val text: String, val graphic: Node, val contextMenu: ContextMenu)
+data class CachedFileCell(val text: String, val graphic: Node, val contextMenu: ContextMenu, val action: () -> Unit)
 
